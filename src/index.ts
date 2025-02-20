ccb9bc324ebb0abb37d27676429628ada9d44ea3
Imranalikm <imranalikm@gmail.com>
2025-02-20 12:56:44 +0530

    feat:folder structuring

---
+++ src/index.ts
@@ -0,0 +1,30 @@
+import express,{Request,Response} from 'express'
+import cookieParser from 'cookie-parser'
+import cors from "cors"
+import dotenv from "dotenv"
+
+
+dotenv.config();
+
+const app =express();
+
+app.use(cors());
+app.use(cookieParser());
+app.use(express.json());
+app.use(express.urlencoded({extended:false}));
+
+
+
+const port = process.env.PORT || 5000
+
+
+// app.get('/',(req:Request,res:Response)=>{
+//     res.send({message:"Hi"}).status(200)
+// })
+
+
+
+
+app.listen(port,()=>{
+    console.log(`server is running on http://localhost:${port}`);   
+})
